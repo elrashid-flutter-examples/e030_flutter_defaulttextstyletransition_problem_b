9a7fb04ea714ced597c19afd3ad6d2681e393314
moh <osman.elrashid@gmail.com>
2019-08-07 09:32:25 +0400

    initial files

---
+++ android/app/src/main/java/com/example/e030_flutter_defaulttextstyletransition_problem_b/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.e030_flutter_defaulttextstyletransition_problem_b;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ git.init.bat
@@ -0,0 +1,5 @@
+Set HUB_PROTOCOL=ssh 
+git init 
+git add . && git commit -m "initial files" 
+hub create elrashid-flutter-examples/e030_flutter_defaulttextstyletransition_problem_b 
+git push --set-upstream origin master 
